d30defeb4033b6c2cb3b6c653db9fd74d608db1a
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-02-04 13:02:05 +0700

    feat: postForm now update summary

--- api/index.ts
+++ api/index.ts
@@ -1,42 +1,28 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import bodyParser from "body-parser";
-import {
-  getNIM,
-  getHello,
-  postFormNIM,
-  populate,
-  getRandom,
-  getSearch,
-} from "../controllers";
+import { getNIM, getHello, postFormNIM, populate, getRandom, getSearch } from "../controllers";
 import cors from "cors";
 
 const app = require("express")();
 
-const allowCors =
-  (fn: Function) => async (req: VercelRequest, res: VercelResponse) => {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // another common pattern
-    // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
-    if (req.method === "OPTIONS") {
-      res.status(200).end();
-      return;
-    }
-    return await fn(req, res);
-  };
+const allowCors = (fn: Function) => async (req: VercelRequest, res: VercelResponse) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
 const jsonParser = bodyParser.json();
 app.use(cors());
 app.get("/api", allowCors(getHello));
-app.get("/api/:NIM", allowCors(getNIM));
+app.get("/api/user/:NIM", allowCors(getNIM));
 app.post("/api/form/:NIM", jsonParser, postFormNIM);
 app.post("/api/users/populate", allowCors(populate));
 app.get("/api/users/random", allowCors(getRandom));
--- controllers/postFormNIM.ts
+++ controllers/postFormNIM.ts
@@ -5,21 +5,60 @@ export default async function postFormNIM(req: any, res: any) {
   // setup
   const NIM = req.body.NIM;
   const reviewerID = req.body.reviewerID;
-  let review: any = {
+  let NewReview: any = {
     comStyle: req.body.comStyle,
     rating: req.body.rating,
     timeStamp: Timestamp.now().toDate().toString(),
   };
-  console.log(NIM, reviewerID, review);
+  console.log(NIM, reviewerID, NewReview);
+
   // at this point, the data is valid
   const { db } = getDB();
-  const reviewRef = db
-    .collection("users")
-    .doc(NIM)
-    .collection("reviews")
-    .doc(reviewerID);
+  const reviewRef = db.collection("users").doc(NIM).collection("reviews").doc(reviewerID);
 
-  const respond = await reviewRef.update(review, { merge: true });
+  // add the review
+  reviewRef.set(NewReview, { merge: true });
 
-  res.send(respond);
+  // update the review summary field
+  const reviewSummaryRef = db.collection("users").doc(NIM);
+  const reviewSummarySnapshot = await reviewSummaryRef.get();
+  const reviewsSnapshot = await reviewSummarySnapshot.ref.collection("reviews").get();
+
+  const reviewSummary = reviewSummarySnapshot.data();
+  const reviews = reviewsSnapshot.docs.map((doc: any) => doc.data());
+
+  console.log("reviewSummary", reviewSummary);
+  console.log("reviews", reviews);
+  // recount rating and comstyle. com style is determined by the majority of the comStyle
+  let totalRating = 0;
+  let comStyleCount: any = {};
+  let totalReviews = reviews.length;
+  reviews.forEach((review: any) => {
+    totalRating += review.rating;
+    if (comStyleCount[review.comStyle]) {
+      comStyleCount[review.comStyle] += 1;
+    } else {
+      comStyleCount[review.comStyle] = 1;
+    }
+  });
+  let comStyle: string = reviewSummary.comStyle;
+  let maxCount = 0;
+  for (let key in comStyleCount) {
+    if (comStyleCount[key] > maxCount) {
+      comStyle = key;
+      maxCount = comStyleCount[key];
+    }
+  }
+  const updatedValue = {
+    rating: totalRating / totalReviews,
+    comStyle: comStyle,
+    totalReviews: totalReviews,
+    lastUpdated: Timestamp.now().toDate().toString(),
+  };
+  console.log("updatedValue", updatedValue);
+
+  // update the review summary
+  reviewSummaryRef.set(updatedValue, { merge: true });
+
+  res.send({});
 }
