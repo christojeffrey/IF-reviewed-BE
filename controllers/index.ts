861b2d52bd7da71e867ee23137e091b8f2d60264
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-02-10 15:58:53 +0700

    feat: deleting populate to prevent bad duse

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,8 +1,7 @@
 import getHello from "./hello";
 import getNIM from "./getNIM";
 import postFormNIM from "./postFormNIM";
-import populate from "./populate";
 import getRandom from "./getRandom";
 import getSearch from "./getSearch";
 import getReview from "./getReview";
-export { getHello, getNIM, postFormNIM, populate, getRandom, getSearch, getReview };
+export { getHello, getNIM, postFormNIM, getRandom, getSearch, getReview };
